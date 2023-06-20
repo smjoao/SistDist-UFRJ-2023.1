d09091391e2be9994449ffa950286f602c0cfd19
Joao Seda <joao_ps@dcc.ufrj.br>
2023-06-20 19:47:32 -0300

    (Trab1) Criada pasta para Trabalho 1

---
+++ Trab1/interface.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import Callable, TypeAlias
+from dataclasses import dataclass
+
+import rpyc # type: ignore
+
+UserId: TypeAlias = str
+
+Topic: TypeAlias = str
+
+# Isso é para ser tipo uma struct
+# Frozen diz que os campos são read-only
+@dataclass(frozen=True, kw_only=True, slots=True)
+class Content:
+    author: UserId
+    topic: Topic
+    data: str
+
+FnNotify: TypeAlias = Callable[[list[Content]], None]
+
+class BrokerService(rpyc.Service): # type: ignore
+
+    # Não é exposed porque só o "admin" tem acesso
+    def create_topic(self, id: UserId, topicname: str) -> Topic:
+        assert False, "TO BE IMPLEMENTED"
+
+    # Handshake
+
+    def exposed_login(self, id: UserId, callback: FnNotify) -> bool:
+        assert False, "TO BE IMPLEMENTED"
+
+    # Query operations
+
+    def exposed_list_topics(self) -> list[Topic]:
+        assert False, "TO BE IMPLEMENTED"
+
+    # Publisher operations
+
+    def exposed_publish(self, id: UserId, topic: Topic, data: str) -> bool:
+        """
+        Função responde se Anúncio conseguiu ser publicado
+        """
+        assert False, "TO BE IMPLEMENTED"
+
+    # Subscriber operations
+
+    def exposed_subscribe_to(self, id: UserId, topic: Topic) -> bool:
+        """
+        Função responde se `id` está inscrito no `topic`
+        """
+        assert False, "TO BE IMPLEMENTED"
+
+    def exposed_unsubscribe_to(self, id: UserId, topic: Topic) -> bool:
+        """
+        Função responde se `id` não está inscrito no `topic`
+        """
+        assert False, "TO BE IMPLEMENTED"
